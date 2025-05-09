95adc55fcff6cbdd40f74b0345adcc2689007eea
thedistromaker <pan.savvy.35@gmail.com>
2025-05-09 17:01:44 +0100

    Update easyarch2.sh

--- easyarch2.sh
+++ easyarch2.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Stage 2. Continuing..."
 systemctl enable sddm iwd dhcpcd
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
 read -p "Base system configured. Enter username:" usernamesel
 useradd $usernamesel
 usermod -aG wheel $usernamesel
